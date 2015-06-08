769568cc66cb20af244dbfaf31234c9b296a1cc6
Rob <rdaly1490@gmail.com>
2015-06-08 13:14:26 -0500

    initial commit

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,7 +1,12 @@
+var newUser = new UserModel();
+
+$(document).ready(function() {
+
 var user = new UserModel();
 var App = Backbone.Router.extend({
 	routes: {
 		'': 'profile',
+		"profile": "profile",
 		'edit': 'edit'
 	},
 	profile: function() {
@@ -16,3 +21,31 @@ var App = Backbone.Router.extend({
 
 var app = new App();
 Backbone.history.start();
+
+// var newUser = new UserModel();
+
+updateUser(newUser);
+
+newUser.on("change", updateUser);
+
+function updateUser(UserModel) {
+	console.log("initial user")
+	$(".profile-usertitle-name").html(UserModel.get("name"));
+	$(".dropdown .dropdown-toggle").html(UserModel.get("name"));
+	$(".profile-usertitle-job").html(UserModel.get("role"));
+	$(".profile-usertitle-email").html(UserModel.get("email"));
+}
+
+$(".form-horizontal").on("submit", function(e) {
+	e.preventDefault();
+	console.log("Submit");
+	newUser.set({
+		name: $("#name").val(),
+		email: $("#inputEmail3").val(),
+		role: $("#role").val()
+	});
+});
+
+
+
+});
