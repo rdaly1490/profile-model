b5b715fe3c75cfa380012c2be6074dd383e77e88
Rob <rdaly1490@gmail.com>
2015-06-08 14:32:05 -0500

    hard mode working kinda

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,8 +1,22 @@
 var newUser = new UserModel();
 
+window.onload = function() {
+		$.get ("http://tiny-pizza-server.herokuapp.com/collections/robd",
+		setHtml,
+		"json"
+		);
+
+	function setHtml(data) {
+		console.log(data[1]);
+		$(".profile-usertitle-name").html(data[1].name);
+		$(".dropdown .dropdown-toggle").html(data[1].name);
+		$(".profile-usertitle-job").html(data[1].role);
+		$(".profile-usertitle-email").html(data[1].email);
+	}
+}
+
 $(document).ready(function() {
 
-var user = new UserModel();
 var App = Backbone.Router.extend({
 	routes: {
 		'': 'profile',
@@ -22,30 +36,51 @@ var App = Backbone.Router.extend({
 var app = new App();
 Backbone.history.start();
 
-// var newUser = new UserModel();
-
 updateUser(newUser);
-
 newUser.on("change", updateUser);
 
 function updateUser(UserModel) {
-	console.log("initial user")
 	$(".profile-usertitle-name").html(UserModel.get("name"));
 	$(".dropdown .dropdown-toggle").html(UserModel.get("name"));
 	$(".profile-usertitle-job").html(UserModel.get("role"));
 	$(".profile-usertitle-email").html(UserModel.get("email"));
+	var myObj =	{
+		name: UserModel.get("name"),
+		email: UserModel.get("email"),
+		role: UserModel.get("role")
+	};
+	$.post
+	("http://tiny-pizza-server.herokuapp.com/collections/robd",
+	myObj,
+	"json"
+	);
+
 }
 
 $(".form-horizontal").on("submit", function(e) {
 	e.preventDefault();
-	console.log("Submit");
 	newUser.set({
 		name: $("#name").val(),
 		email: $("#inputEmail3").val(),
 		role: $("#role").val()
 	});
+
 });
 
+// window.onload = function() {
+// 				$.get ("http://tiny-pizza-server.herokuapp.com/collections/robd",
+// 			setHtml,
+// 			"json"
+// 			);
+
+// 	function setHtml(data) {
+// 		console.log(data[1]);
+// 		$(".profile-usertitle-name").html(data[1].name);
+// 		$(".dropdown .dropdown-toggle").html(data[1].name);
+// 		$(".profile-usertitle-job").html(data[1].role);
+// 		$(".profile-usertitle-email").html(data[1].email);
+// 	}
+// }
 
 
 });
